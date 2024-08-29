ad6dbf11de579de4e60b4fffba1d05efe73b3f0a
Mehedi_office <my.office@example.com>
2024-08-29 10:49:16 +0600

    sqlite database created

--- run.py
+++ run.py
@@ -1,11 +1,6 @@
-from flask import Flask, render_template
-app = Flask(__name__)
- 
- 
-@app.route("/")
-def index():
-    return render_template("index.html")
- 
-if __name__ == "__main__":
-    app.run()
+from app import create_app
 
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
